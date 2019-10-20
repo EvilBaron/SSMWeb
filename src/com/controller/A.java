91bb5061b53acfdbcbebb91ad2f6fffb3cf82826
EvilBaron <lidiandian95@163.com>
2019-10-20 16:50:40 +0800

    V0.10

--- src/com/controller/A.java
+++ src/com/controller/A.java
@@ -2,6 +2,6 @@ package com.controller;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("Hello Git! -v0.7 -master update ---merge合并分支  --合并reking分支");
+		System.out.println("Hello Git! + github!");
 	}
 }
